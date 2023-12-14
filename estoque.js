a1ce2b646e17fde6ea38cd78bf4e05a34eba5760
Breno <breno.rosado@estudante.ufjf.br>
2023-12-13 21:06:19 -0300

    reformata a função transacao

--- estoque.js
+++ estoque.js
@@ -18,7 +18,52 @@ function getEstoque(){
 }
 
 function transacao(origem, destino, quantidade, fruta){
-    if(origem === 'pomar'){
-        estoque[destino][0].qtd += quantidade;
+    if (origem === destino) {
+        return;
     }
+
+    if(quantidade <= 0){
+        return;
+    }
+    
+    if(origem === 'pomar'){
+        dePomarParaPessoa(destino, quantidade, fruta);
+    }
+
+    if(destino === "pomar"){
+        dePessoaParaPomar(origem, quantidade, fruta);
+    }
+}
+
+function dePomarParaPessoa(destino, quantidade, fruta) {
+    const pessoa = estoque[destino];
+    let monte;
+    for(let i=0; i < pessoa.length; i++){
+        if(pessoa[i].tipo === fruta){
+            monte = pessoa[i];
+            break;
+        }
+    }
+    if(!monte){
+        monte = {tipo: fruta, qtd: 0};
+        pessoa.push(monte);
+    }
+
+    monte.qtd += quantidade;
+    return;
+}
+
+function dePessoaParaPomar(origem, quantidade, fruta) {
+    const pessoa = estoque[origem];
+        let monte;
+        for(let i=0; i < pessoa.length; i++){
+            if(pessoa[i].tipo === fruta){
+                monte = pessoa[i];
+                break;
+            }
+        }
+        if(!monte){
+            return;
+        }
+        monte.qtd -= Math.min(quantidade, monte.qtd);
 }
